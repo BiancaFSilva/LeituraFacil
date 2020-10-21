9fc8a37dff130405817fc4bf5d5e5879bda7e173
BiancaFSilva <biancaflorianodasilva@gmail.com>
2020-10-21 16:46:05 -0300

    Exemplo

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,18 +21,10 @@
         autoHeight: false,
         autoplay: true,
         responsive: {
-            992: {
-                items: 4,
-            },
-            768: {
-                items: 3,
-            },
-            576: {
-                items: 2,
-            },
-            0: {
-                items: 1,
-            }
+            992: { items: 4, },
+            768: { items: 3, },
+            576: { items: 2, },
+            0: { items: 1, }
         }
     });
 
