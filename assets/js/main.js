123d05e861ac3a06e181f2b0980a19a7d86c4bb8
BiancaFSilva <biancaflorianodasilva@gmail.com>
2020-10-21 16:34:39 -0300

    carrossel

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,35 @@
         $("#preloder").delay(200).fadeOut("slow");
     });
 
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    $(".livros_slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 4,
+            },
+            768: {
+                items: 3,
+            },
+            576: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            }
+        }
+    });
+
 })(jQuery);
